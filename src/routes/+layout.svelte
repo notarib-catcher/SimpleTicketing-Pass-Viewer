f05026508706dee4cb33db6fc747f97ab0a2fafc
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-08-08 20:11:50 +0530

    Trying to fix issue with safari height changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     import Footer from "$lib/Footer.svelte";
 </script>
 
-<div class=" relative h-screen w-screen bg-gradient-to-t from-[#35180a] via-[#180524] to-[#2c0418] flex flex-row items-center justify-center">
+<div class=" relative h-[100vh] w-screen bg-gradient-to-t from-[#35180a] via-[#180524] to-[#2c0418] flex flex-row items-center justify-center">
     <slot />
     <div class="absolute bottom-0">
         <Footer />
