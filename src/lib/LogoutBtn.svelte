175c155b7ae3a5fe4a65aeeb2862cd912225b689
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-08-08 22:12:52 +0530

    Updated fonts on the logout btn and username display

--- src/lib/LogoutBtn.svelte
+++ src/lib/LogoutBtn.svelte
@@ -3,10 +3,10 @@
     export let name;
 </script>
 <div class="fixed top-1 right-1 flex flex-row items-center">
-    <div class=" font-bold bg-[#dad9ca] max-sm:fixed max-sm:left-1 max-sm:top-3 max-sm:w-screen bg-clip-text text-transparent">
+    <div class=" font-bold bchcry bg-[#dad9ca] max-sm:fixed max-sm:left-2 max-sm:top-3 max-sm:w-screen bg-clip-text text-transparent">
         Welcome, {name}
     </div>
-    <button class=" ml-3 bg-opacity-5 p-2 rounded-xl text-white hover:bg-white hover:bg-opacity-10 bg-white focus:bg-opacity-10 transition-all duration-300 font-light " on:click={() => {signOut({callbackUrl:"/"})}}>
+    <button class=" mr-1 ml-2 bg-opacity-5 p-2 rounded-xl bchcry uppercase text-white hover:bg-white hover:bg-opacity-10 bg-white focus:bg-opacity-10 transition-all duration-300 font-light " on:click={() => {signOut({callbackUrl:"/"})}}>
         Logout
     </button>
 </div>
