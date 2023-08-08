1470694409d71e00d11813d4078b7dfd2cfc57ec
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-08-08 22:09:34 +0530

    fixed username and logout box color. Adjusted their styling on small screens

--- src/lib/LogoutBtn.svelte
+++ src/lib/LogoutBtn.svelte
@@ -3,10 +3,10 @@
     export let name;
 </script>
 <div class="fixed top-1 right-1 flex flex-row items-center">
-    <div class=" font-bold bg-gradient-to-r  from-[#e4c359] to-[#e4ab32] bg-clip-text text-transparent">
+    <div class=" font-bold bg-[#dad9ca] max-sm:fixed max-sm:left-1 max-sm:top-3 max-sm:w-screen bg-clip-text text-transparent">
         Welcome, {name}
     </div>
-    <button class=" ml-3 bg-opacity-5 p-2 rounded-xl text-white hover:bg-white hover:bg-opacity-10 bg-white focus:bg-opacity-10 transition-all duration-300 font-light italic " on:click={() => {signOut({callbackUrl:"/"})}}>
+    <button class=" ml-3 bg-opacity-5 p-2 rounded-xl text-white hover:bg-white hover:bg-opacity-10 bg-white focus:bg-opacity-10 transition-all duration-300 font-light " on:click={() => {signOut({callbackUrl:"/"})}}>
         Logout
     </button>
 </div>
