1b07aa6c8c6f13a6ee79feaaabb69452572d7eea
D+ADI <15999680+notarib-catcher@users.noreply.github.com>
2023-08-09 03:49:26 +0000

    Added pulse animation to logout to indicate loading

--- src/lib/LogoutBtn.svelte
+++ src/lib/LogoutBtn.svelte
@@ -6,7 +6,7 @@
     <div class=" font-bold bchcry bg-[#dad9ca] max-sm:fixed max-sm:left-2 max-sm:top-3 max-sm:w-screen bg-clip-text text-transparent">
         Welcome, {name}
     </div>
-    <button class="mr-1 ml-2 bg-opacity-5 p-2 rounded-xl bchcry uppercase text-white hover:bg-white hover:bg-opacity-10 bg-white focus:bg-opacity-10 transition-all duration-300 font-light " on:click={() => {signOut({callbackUrl:"/"})}}>
+    <button class="focus:animate-pulse mr-1 ml-2 bg-opacity-5 p-2 rounded-xl bchcry uppercase text-white hover:bg-white hover:bg-opacity-10 bg-white focus:bg-opacity-10 transition-all duration-300 font-light " on:click={() => {signOut({callbackUrl:"/"})}}>
         Logout
     </button>
 </div>
