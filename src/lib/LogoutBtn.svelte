84dd01e2882389de233c63e8638306f3fc6620dd
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-08-11 19:17:55 +0530

    Prevent div from covering logout button

--- src/lib/LogoutBtn.svelte
+++ src/lib/LogoutBtn.svelte
@@ -8,7 +8,7 @@
     }
 </script>
 <div class="fixed top-1 right-1 flex flex-row items-center loadinwithimg">
-    <div class=" font-bold bchcry bg-[#dad9ca] max-sm:fixed max-sm:left-2 max-sm:top-3 max-sm:w-screen bg-clip-text text-transparent">
+    <div class=" font-bold bchcry bg-[#dad9ca] max-sm:fixed max-sm:left-2 max-sm:top-3 bg-clip-text text-transparent">
         Greetings, {name}
     </div>
     <button class="focus:animate-pulse mr-1 ml-2 bg-opacity-5 p-2 rounded-xl bchcry uppercase text-white hover:bg-white hover:bg-opacity-10 bg-white focus:bg-opacity-10 transition-all duration-300 font-light flex flex-row " on:click={onClickLogout}>
